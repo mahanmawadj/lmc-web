e2e83ec13416d610e9e288be93b136635400603e
mahanmawadj <mahanmawadj@hotmail.com>
2016-09-24 14:16:18 -0400

    changed to express 3 start format

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿
+﻿var debug = require('debug')('lmcweb');
 /**
  * Module dependencies.
  */
@@ -67,7 +67,11 @@ app.use(function (err, req, res, next) {
     });
 });
 
-module.exports = app;
+app.set('port', process.env.PORT || 80);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port: ' + server.address().port);
+});
 
 
 
